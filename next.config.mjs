eecfebf52bff9c735668ae4168a0f5c9ab778b58
Ahmed Elsankary <elsankary99@gmail.com>
2026-07-04 22:01:44 +0300

    Remove GitHub Pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
+export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-  trailingSlash: true,
 };
 
 export default nextConfig;
